d20607443fd4ada50fab79312955acb4db9629aa
Jackinkade <66413815+Jackinkade@users.noreply.github.com>
2020-06-27 00:17:29 +0600

    Update index.js

--- training/index.js
+++ training/index.js
@@ -1,36 +1,39 @@
 'use strict';
 
 const article = document.querySelector("article");
-let text = prompt("Введите название для вашего нового элемента");
+
 const DomElement = {
-  selector : text,
-  height : 100,
-  width : 1000,
-  bg: "#9A8DE1",
-  fontSize: 50,
+  selector : prompt("??") ,
+  height : 300,
+  width : 300,
+  bg: "blue",
+  fontSize: 60,
 
     createElement : function() {
-     
+      let text = prompt("Введите название для вашего нового элемента");
         if(this.selector[0] === "."){
+         
             let div = document.createElement("div");
               div.textContent = text;
               div.classListAdd = this.selector.slice(1);
 
-              div.style.cssText = "color" + this.bg + "height= " + this.height + "px" + 
-                "width= " + this.width +"px" + "font-size= " + this.fontSize + "px";
-
+              div.style.cssText = 'color:' + this.bg + '; background-color: black; width: ' + 
+              this.width + 'px; height: ' + this.height + 'px; font-size: ' + this.fontSize + 'px;';
+            
           article.appendChild(div);
+          div.classList.add(text);
         
         }else if(this.selector[0] === "#"){
+          
             let p = document.createElement("p");
               p.textContent = text;
               p.classListAdd = this.selector.slice(1);
 
-              p.style.cssText = "color" + this.bg + "height= " + this.height + "px" + 
-                "width= " + this.width +"px" + "font-size= " + this.fontSize + "px";
-
+              p.style.cssText = 'color:' + this.bg + '; background-color: black; width: ' + 
+              this.width + 'px; height: ' + this.height + 'px; font-size: ' + this.fontSize + 'px;';
+            
           article.appendChild(p);
-
+          p.id = text;
         }
     }
 
