62834e66a76c39c3b46cddfbe462d63f4ec70679
Jackinkade <66413815+Jackinkade@users.noreply.github.com>
2020-06-27 22:46:58 +0600

    Update index.js

--- lesson15part2/index.js
+++ lesson15part2/index.js
@@ -6,12 +6,11 @@ class First{
   hello() { console.log(this.sentence); }
 }
 class Second extends First{
-
-newHello (){console.log("А я наследуемый метод!");}
+  
+hello (){
+  super.hello();
+  console.log("А я наследуемый метод!");}
 }
-
-const first = new First();
-first.hello();
 const second = new Second();
-second.hello();
-setTimeout(second.newHello(), 200);
+
+setTimeout(second.hello(), 200);
