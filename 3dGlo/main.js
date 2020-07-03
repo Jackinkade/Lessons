f184ed1ff0664fc8a01855026323ddb95f49c7e6
Jackinkade <66413815+Jackinkade@users.noreply.github.com>
2020-07-03 10:35:34 +0600

    rrer

--- 3dGlo/main.js
+++ 3dGlo/main.js
@@ -46,4 +46,92 @@ window.addEventListener('DOMContentLoaded', () => {
         };
         updateClock();
     }
-    countTimer('1 july 2020');
+    countTimer('2 july 2020');
+    //menu
+    
+    const toggleMenu = () => {
+
+        const btnMenu = document.querySelector('.menu'),
+            menu = document.querySelector('menu'),
+            closeBtn = document.querySelector('.close-btn'),
+            menuItem = document.querySelectorAll('ul>li');
+        // document.addEventListener('click', event => {
+            
+        //     if (!btnMenu.contains(event.target)) btnMenu.style.display = 'block';
+        // });
+        const handlerMenu = () => {
+            menu.classList.toggle('active-menu');
+        };
+        btnMenu.addEventListener('click', handlerMenu);
+        closeBtn.addEventListener('click', handlerMenu);
+        // eslint-disable-next-line arrow-parens
+        menuItem.forEach((elem) => elem.addEventListener('click', handlerMenu));
+       
+    };
+    toggleMenu();
+
+    //popup
+
+    const togglePopUp = () => {
+        const popup = document.querySelector('.popup'),
+            popupBtn = document.querySelectorAll('.popup-btn'),
+            popupClose = document.querySelector('.popup-close'),
+            popupContent = document.querySelector('.popup-content');
+        // let count = 0;
+        // let popupanimate;
+        let count = 0,
+            count1 = 100;
+
+        const openPopup = () => {
+            const popupanimate = requestAnimationFrame(openPopup);
+            // count++;
+            count1 += 10;
+            popup.style.display = 'block';
+            if (popup.style.opacity < 5) {
+                // popup.style.display = 'block';
+              
+                popup.style.opacity = count += 0.155;
+                popupContent.style.left = count1 * 2 + 'px';
+                // 1 - Math.sin(Math.acos(count1));
+            // if (count < 600) {
+            //     popup.style.display = 'block';
+            //     popupContent.style.left = count * 2 + 'px';
+            } else {
+                cancelAnimationFrame(popupanimate);
+            }
+        };
+        // popupanimate = requestAnimationFrame(openPopup);
+        
+        popupBtn.forEach(elem => {
+            elem.addEventListener('click', () => {
+                if (window.innerWidth  >= 768) {
+                    openPopup();
+                } else {
+                    popup.style.display = '';
+                }
+            });
+
+            popupClose.addEventListener('click', () => {
+                popup.style.display = '';
+            });
+           
+        });
+        
+        // document.querySelectorAll('.popup-btn').addEventListener('click', () => {
+        //     const popup = document.querySelector('popup');
+            
+        //     let movePopup;
+        //     let count = 0;
+        //     const movePopupBlock = () => {
+        //         movePopup = requestAnimationFrame(movePopupBlock);
+        //         count++;
+        //         if (count < 500) {
+        //             popup.style.left = count + 'px';
+        //         } else if (count < 700) {
+        //             cancelAnimationFrame(movePopup);
+        //         }
+        //     };
+        //     movePopupBlock();
+        // });
+    };
+    togglePopUp();
