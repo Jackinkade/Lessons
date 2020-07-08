bad2332666bd54b2ac5cb516a76a7ed2aa397cb6
Jackinkade <66413815+Jackinkade@users.noreply.github.com>
2020-07-08 15:48:14 +0600

    calc

--- 3dGlo/main.js
+++ 3dGlo/main.js
@@ -44,7 +44,7 @@ window.addEventListener('DOMContentLoaded', () => {
         };
         updateClock();
     }
-    countTimer('1 july 2020');
+    countTimer('9 july 2020');
     //menu
     
     const toggleMenu = () => {
@@ -241,6 +241,7 @@ window.addEventListener('DOMContentLoaded', () => {
     };
     
     slider();
+
     // меняем фото 
     const changePhoto = () => {
         const commandPhoto = document.querySelectorAll('.command__photo');
@@ -268,7 +269,56 @@ window.addEventListener('DOMContentLoaded', () => {
         });
     };
     deleteWord();
+
+    // калькулятор 
+    const calc = (price = 100) => {
+
+        const calcBlock = document.querySelector('.calc-block'),
+            calcType = document.querySelector('.calc-type'),
+            calcSquare = document.querySelector('.calc-square'),
+            calcDay = document.querySelector('.calc-day'),
+            calcCount = document.querySelector('.calc-count'),
+            totalValue = document.querySelector('total');
+        const countSum = () => {
+            let total = 0,
+                countValue = 1,
+                dayValue = 1;
+
+            const typeValue = calcType.options[calcType.selectedIndex].value,
+                squareValue = +calcSquare.value;
+            
+            if (calcCount.value > 1) {
+                countValue += (calcCount.value - 1) / 10;
+            }
+
+            if (calcDay.value && calcDay.value < 5) {
+                dayValue *= 2;
+            } else if (calcDay.value && calcDay < 10) {
+                dayValue * 1.5;
+            }
+            
+            if (typeValue && squareValue) {
+                total = price * typeValue * squareValue * countValue * dayValue;
+            }
+            totalValue.textContent = total;
+           
+        };
+
+        calcBlock.addEventListener('change', event => {
+            const target = event.target;
+  
+            if (target === calcType || target === calcSquare || target === calcDay || target === calcCount) {
+                countSum();
+            }
+        });
+
+
+    };
+    calc(100);
         
 });
+
+
+
 
 
